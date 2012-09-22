02ceb1766f04ef98f0787fbb11737ad3c5dd32ee
Gareth Rees <gareth.h.rees@gmail.com>
2012-09-23 00:41:42 +0100

    Friendly tag urls

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -2,8 +2,8 @@ ActsAsTaggableOn.remove_unused_tags = true
 ActsAsTaggableOn.force_lowercase = true
 ActsAsTaggableOn.force_parameterize = true
 
-# ActsAsTaggableOn::Tag.class_eval do
-#   def to_param
-#     name.parameterize
-#   end
-# end
+ActsAsTaggableOn::Tag.class_eval do
+  def to_param
+    name.parameterize
+  end
+end
